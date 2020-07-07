21a1ee8991720c4eab967e2be7a815add30ac741
swsbmm <swsbmm@gmail.com>
2020-07-07 11:54:11 -0500

    version mejorada, funcionando al parecer bien

--- Script.js
+++ Script.js
@@ -1,21 +1,21 @@
 function calcular(infija) {
     let posfija = infAPosPlus(infija)
-
-    let str = ''
-    for(var i=0; i<posfija.size(); i++){
-        str += posfija.get(i).info + "   "
-        
-    }
-
     let resultado = evaluarPlus(posfija)
-
+    console.log(resultado)
+    if(isNaN(resultado)){
+        resultado = 'Faltan datos'
+    }
+    if(resultado == Number.POSITIVE_INFINITY){
+        resultado = 'No se puede efectuar la operacion.'
+    }
     data = {
         posfijo: posfija.getInfo(),
         resultado
     }
-    
-    console.log(str)
-    console.log(evaluarPlus(posfija))
+    let prueba = "."
+    console.log(`el hascode de ${prueba} es  ${prueba.hashCode()}`)
+    console.log('Posfija: ', posfija.getInfo())
+    console.log('Resultado: ', resultado)
     return data
 }
 
@@ -23,65 +23,117 @@ function calcular(infija) {
 
 
 function infAPosPlus(infija) {
-    let pila = new Pila()
-    let posfija = new Lista()
-    let letra = ''
-    pila.initPm()
-    let list = new Lista()
-
-    for (var j = 0; j < infija.length; j++) {
-        let letraUnic = infija[j];
-        if (letraUnic.hashCode() >= 48 && letraUnic.hashCode() <= 57) {
-            letra = letra.concat(letraUnic);
-        } else if (esOperador(letraUnic)) {
-            if(!letra == ''){
-                list.add(letra);
+    let pila = new Pila()  // Se crea la pila 
+    let posfija = new Lista()   //se crea la lista para ir anotando la expresion en posfijo
+    let letra = '' //variable donde voy a guardar temporalmente cada letra de la cadena de string
+    pila.initPm() //hace que la pila este en null
+    let list = new Lista() //otra lista donde se guardaran los elementos de la expresion
+    let agregarPunto = true
+    for (let j = 0; j < infija.length; j++) { //se recorre la cadena string que ingresa el usuario
+        let letraUnic = infija[j];  // se guarda en esta variable cada char del string
+        if (letraUnic.hashCode() >= 48 && letraUnic.hashCode() <= 57 || letraUnic.hashCode() == 46) {
+            if(letraUnic.hashCode() == 46 && agregarPunto){
+                 letra = letra.concat(letraUnic)
+                agregarPunto = false
+            }else if(letraUnic.hashCode() >= 48 && letraUnic.hashCode() <= 57){
+                letra = letra.concat(letraUnic);
             }
-            list.add(letraUnic);
-            letra = '';
+
+                //con esta comparacion se sabe si es un numero de 0 a 9
+             // si es un numero entonces a letra le concatenamos lo que tenga letra, asi se puede juntar numero de mas de 2 dijitos
+        } else if (esOperador(letraUnic)) { // si encontramos un operador entonces vamos a hacer lo siguiente
+            if (!letra == '') { // si el numero es vacio no guardamos espacios en blanco pero si no entonces
+                list.add(letra); // agregamos el numero que tenemos temporalmente guardada en este caso un operado a list
+            }
+            list.add(letraUnic); // agregamos el operador a la lista
+            letra = ''; // borramos nuestra variable temporal para repetir el procedimiento segun la lontid de la cadena
+            agregarPunto = true
         }
     }
-    list.add(letra);
-
-    // console.log(list.getInfo().info)
-
-    console.log(list.size())
-    for (var i = 0; i < list.size(); i++) {
-        // console.log(list.get(i))
+    list.add(letra); // al final guardamos lo que nos quedo en nuestra expresion.
+    
+/// ALGORITMO DE POR COMPUTADOR FUNCIONAL
+    let aux = ''
+    let i =0
+    let j = -1
+    let elemento 
+    while(i<list.size()){
         let caracter = list.get(i).info
-        if (esOperador(caracter)) {
-            if (pila.vaciaPm()) {
-                pila.insPm(caracter)
-            } else {
-
-                if (prioridadEnExpresion(caracter) > prioridadEnPila(pila.cabeza().info)) {
-                    pila.insPm(caracter)
-                } else {
-                    if (caracter == ')') {
-                        while (pila.cabeza().info !== '(') {
-                            posfija.add(pila.retiraPm())
-                        }
-                        pila.retiraPm()
-                    } else {
-                        posfija.add(pila.retiraPm())
-                        pila.insPm(caracter)
-                    }
-
-
-                }
-
-            }
-        } else {
+        if(!esOperador(caracter)){
+            console.log("QUE HACEN PERRAS")
             posfija.add(caracter)
+           
+        }else{
+            try {
+                console.log(`prioeridad entre ${caracter} Y ${pila.cabeza().info}`)    
+            } catch (error) {
+                
+            }
+            
+            while(!pila.vaciaPm() && prioridadEnExpresion1(caracter,pila.cabeza().info) === 1){
+                elemento = pila.retiraPm()
+                posfija.add(elemento)
+            }
+            if(caracter !== ')'){
+                pila.insPm(caracter)
+            }else{
+                pila.retiraPm()
+            }
+            
         }
+        i++
+    }
+
+    while(!pila.vaciaPm()){
+        elemento = pila.retiraPm()
+        posfija.add(elemento)
     }
 
 
-    while (!pila.vaciaPm()) {
-        posfija.add(pila.retiraPm())
-    }
 
-    return posfija
+    // otro algoritmo pero falla
+
+
+
+    // for (var i = 0; i < list.size(); i++) { // ahora vamos a recorrer la lista simple donde tenemos la expresion ingresada por el usuario
+    //     let caracter = list.get(i).info // en una variable aux llamada caracter vamos a guardar el termino de la expresion y vamos a evaluarlo
+    //     if (esOperador(caracter)) { //si es termino es un operador entonces
+    //         if (pila.vaciaPm()) { // miramos si la pila esta vacia
+    //             pila.insPm(caracter) // si esta vacia entonces metemos el operador
+    //         } else { // si la pila no esta vacia entonces
+
+    //             if (prioridadEnExpresion(caracter) > prioridadEnPila(pila.cabeza().info)) { //vamos a ver la prioridad de los operadores
+    //                 pila.insPm(caracter) //miramos si el operador es de mayor gerarquia que el que se encuentra en la cabeza de la pila
+    //                 // en caso de que sea mayor entonces se agrega el operador a la pila
+    //             } else { // si el grado es menor entonces
+    //                 if (caracter == ')') { // miramos si el caracter es un corchete a cerrar, si se cumple entonces
+    //                     while (pila.cabeza().info !== '(') {
+    //                          //pasamos todo lo que tengamos en la pila a posfijo 
+    //                         posfija.add(pila.retiraPm()) //hasta que encontremos un corchete que abre o hasta que la pila quede vacia
+    //                     }
+    //                     let a = pila.retiraPm() // borramos el corchete que queda
+    //                 } else { // en caso de que no sea un corchete que cierra entonces
+    //                     posfija.add(pila.retiraPm()) // agregamos el a posfijo el ultimo elemento de la lista
+    //                     pila.insPm(caracter) // y ponemos el nuevo operador
+    //                 }
+    //             }
+
+    //         }
+    //     } else {// si no es un operador 
+    //         if (!caracter == '') {// si el caracter no esta vacio
+    //             posfija.add(caracter) // agregamos a posfijo el caracter
+    //         }
+
+    //     }
+
+    // }
+
+
+    // while (!pila.vaciaPm()) { // este while pasa todo lo de pila a posfijo
+    //     posfija.add(pila.retiraPm())
+    // }
+
+     return posfija //retornamos la lista posfija
 }
 
 
@@ -127,21 +179,25 @@ function infAPos(infija) {
 
 
 function evaluarPlus(posfija) {
-    let pila = new Pila()
-    pila.initPm()
-    for (var i = 0; i < posfija.size(); i++) {
-        let caracter = posfija.get(i).info
-        console.log(caracter)
-        if (!esOperador(caracter)) {
-            let num = parseFloat(caracter)
-            pila.insPm(num)
-        } else {
-            let num2 = pila.retiraPm();
-            let num1 = pila.retiraPm();
-            let num3 = operacion(caracter, num1, num2)
-            pila.insPm(num3)
+    console.log('LLAMANDO FUNCION')
+    let pila = new Pila() // creammos una pila
+    pila.initPm() // la setiamos en null
+    for (var i = 0; i < posfija.size(); i++) { //recorremos la lista simple de posfija
+        let caracter = posfija.get(i).info // guardamos en caracter los elementos de posfija
+        // console.log(caracter)
+        if (!esOperador(caracter)) { //si el caracter es diferente de un operador
+            let num = parseFloat(caracter) //convertimos el numero a un float 
+            pila.insPm(num) // lo agregamos a la pila
+        } else { // si el caracter es un operador entonces buscaremos los numeros para operarlos
+            let num2 = pila.retiraPm(); // sacamos el numero 2 de la pila
+            let num1 = pila.retiraPm(); // sacamos el numero 1 de la pila
+            let num3 = operacion(caracter, num1, num2) //aplicamos la operacion que hemos encontrado
+            pila.insPm(num3) // agregamos el resultado a la pila
+
+            console.log(`${num1}  ${caracter}  ${num2} = ${num3}`)
         }
-    }
+    }// se repite el proceso, al final el resultado queda en la cabeza de la pila 
+    
     return pila.cabeza().info
 }
 
@@ -176,23 +232,52 @@ function operacion(caracter, num1, num2) {
 
 }
 
+function prioridadEnExpresion1(arreglo,pila) {
+    let numarreglo = 0
+    if (arreglo == '+') { numarreglo = 0 }
+    if (arreglo == '-') { numarreglo = 1 }
+    if (arreglo == '*') { numarreglo = 2 }
+    if (arreglo == '/') { numarreglo = 3 }
+    if (arreglo == '^') { numarreglo = 4 }
+    if (arreglo == '(') { numarreglo = 5 }
+    if (arreglo == ')') { numarreglo = 6 }
+
+    
+    let numpila = 0
+    if (pila == '+') { numpila = 0 }
+    if (pila == '-') { numpila = 1 }
+    if (pila == '*') { numpila = 2 }
+    if (pila == '/') { numpila = 3 }
+    if (pila == '^') { numpila = 4 }
+    if (pila == '(') { numpila = 5 }
+
+    let matrizJerarquia = [ [1,1,0,0,0,0,1],
+                            [1,1,0,0,0,0,1],
+                            [1,1,1,1,0,0,1],
+                            [1,1,1,1,0,0,1],
+                            [1,1,1,1,1,0,1],
+                            [0,0,0,0,0,0,0],]
+
+    return matrizJerarquia[numpila][numarreglo]                        
+}
+
 function prioridadEnExpresion(operador) {
-    if (operador == '+') { return 1 }
-    if (operador == '-') { return 1 }
-    if (operador == '*') { return 2 }
-    if (operador == '/') { return 2 }
-    if (operador == '^') { return 4 }
-    if (operador == '(') { return 5 }
+    if (operador == '+') { return 2 }
+    if (operador == '-') { return 2 }
+    if (operador == '*') { return 3 }
+    if (operador == '/') { return 3 }
+    if (operador == '^') { return 5 }
+    if (operador == '(') { return 6 }
     if (operador == ')') { return 0 }
 
 }
 
 function prioridadEnPila(operador) {
-    if (operador == '+') { return 1 }
-    if (operador == '-') { return 1 }
-    if (operador == '*') { return 2 }
-    if (operador == '/') { return 2 }
-    if (operador == '^') { return 3 }
+    if (operador == '+') { return 2 }
+    if (operador == '-') { return 2 }
+    if (operador == '*') { return 3 }
+    if (operador == '/') { return 3 }
+    if (operador == '^') { return 4 }
     if (operador == '(') { return 0 }
     if (operador == ')') { return 0 }
 
